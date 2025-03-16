33c4026a2b80f9f251744c4a936fdaa95486bffd
Shota FUJI <pockawoooh@gmail.com>
2025-03-16 16:35:24 +0900

    Remove unused functions

--- routes/util.go
+++ routes/util.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"io/fs"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -51,54 +49,6 @@ type repoInfo struct {
 	Git      *git.GitRepo
 	Path     string
 	Category string
-}
-
-func (d *deps) getAllRepos() ([]repoInfo, error) {
-	repos := []repoInfo{}
-	max := strings.Count(d.c.Repo.ScanPath, string(os.PathSeparator)) + 2
-
-	err := filepath.WalkDir(d.c.Repo.ScanPath, func(path string, de fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if de.IsDir() {
-			// Check if we've exceeded our recursion depth
-			if strings.Count(path, string(os.PathSeparator)) > max {
-				return fs.SkipDir
-			}
-
-			if d.isIgnored(path) {
-				return fs.SkipDir
-			}
-
-			// A bare repo should always have at least a HEAD file, if it
-			// doesn't we can continue recursing
-			if _, err := os.Lstat(filepath.Join(path, "HEAD")); err == nil {
-				repo, err := git.Open(path, "")
-				if err != nil {
-					log.Println(err)
-				} else {
-					relpath, _ := filepath.Rel(d.c.Repo.ScanPath, path)
-					repos = append(repos, repoInfo{
-						Git:      repo,
-						Path:     relpath,
-						Category: d.category(path),
-					})
-					// Since we found a Git repo, we don't want to recurse
-					// further
-					return fs.SkipDir
-				}
-			}
-		}
-		return nil
-	})
-
-	return repos, err
-}
-
-func (d *deps) category(path string) string {
-	return strings.TrimPrefix(filepath.Dir(strings.TrimPrefix(path, d.c.Repo.ScanPath)), string(os.PathSeparator))
 }
 
 func setContentDisposition(w http.ResponseWriter, name string) {
