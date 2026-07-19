8c0253211db0783714ac5f70340ba3b55d9e66b1
Shota FUJI <pockawoooh@gmail.com>
2026-07-19 10:38:57 +0900

    Move podman devShell code under nix/ directory

--- flake.nix
+++ flake.nix
@@ -105,53 +105,7 @@
           };
 
           # For those who don't have podman on system.
-          podman =
-            let
-              conf = pkgs.stdenv.mkDerivation {
-                name = "podman-config-files";
-
-                # No source.
-                unpackPhase = "true";
-
-                postInstall = ''
-                  mkdir $out
-                  cat > $out/registries.conf <<EOF
-                  unqualified-search-registries = ["docker.io"]
-                  EOF
-
-                  # This is meant to be copied under ~/.config/containers/
-                  cat > $out/policy.json <<EOF
-                  {
-                    "default": [{ "type": "insecureAcceptAnything" }]
-                  }
-                  EOF
-
-                  cat > $out/containers.conf <<EOF
-                  [engine]
-                  cgroup_manager="cgroupfs"
-                  events_logger="file"
-                  EOF
-                '';
-              };
-            in
-            pkgs.mkShell {
-              packages = with pkgs; [
-                podman
-                conf
-              ];
-
-              shellHook = ''
-                if [[ ! -f ~/.config/containers/policy.json && ! -f /etc/containers/policy.json ]]; then
-                  echo "Create policy.json file for podman."
-                  echo "https://podman.io/docs/installation#policyjson"
-                  echo "If you are okay with insecureAcceptAnything for all, run:"
-                  echo "install -Dm555 ${conf}/policy.json ~/.config/containers/policy.json"
-                fi
-              '';
-
-              CONTAINERS_REGISTRIES_CONF = "${conf}/registries.conf";
-              CONTAINERS_CONF = "${conf}/containers.conf";
-            };
+          podman = pkgs.callPackage ./nix/devshell-podman.nix { };
         }
       );
 
